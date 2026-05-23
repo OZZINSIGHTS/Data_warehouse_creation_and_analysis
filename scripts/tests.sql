a9d700aa61814aacc7261753468f91ca5e4465c4
Oussama ZafZaf <oussamazafzaff@gmail.com>
2026-05-23 21:09:42 +0100

    Add SQL script for monthly sales analysis

---
+++ scripts/tests.sql
@@ -0,0 +1,29 @@
+SELECT MONTH(due_date),SUM(sales_amount),
+LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date) ,
+ROUND(SUM(SALES_AMOUNT) - (LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date)) * 100 /NULLIF((LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date))),0)),2)) GROWTH
+FROM gold_layer.fact_sales
+GROUP BY MONTH(due_date)
+
+
+WITH sales_cte AS
+(
+    SELECT 
+        MONTH(due_date) AS month_number,
+        SUM(sales_amount) AS total_sales
+    FROM gold_layer.fact_sales
+    GROUP BY MONTH(due_date)
+)
+
+SELECT 
+    month_number,
+    total_sales,
+
+    LAG(total_sales) OVER(ORDER BY month_number) AS previous_month_sales,
+
+    ROUND(
+        (total_sales - LAG(total_sales) OVER(ORDER BY month_number))
+        * 100.0
+        / NULLIF(LAG(total_sales) OVER(ORDER BY month_number),0)
+    ,2) AS growth_percentage
+
+FROM sales_cte;
