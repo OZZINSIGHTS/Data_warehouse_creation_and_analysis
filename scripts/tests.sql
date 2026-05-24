5adeaed9f2dcc2117d87168cceb72d73e5a8ff22
Oussama ZafZaf <oussamazafzaff@gmail.com>
2026-05-24 11:40:24 +0100

    Fix SQL query for sales growth calculation

--- scripts/tests.sql
+++ scripts/tests.sql
@@ -1,6 +1,7 @@
 SELECT MONTH(due_date),SUM(sales_amount),
 LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date) ,
-ROUND(SUM(SALES_AMOUNT) - (LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date)) * 100 /NULLIF((LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date))),0)),2)) GROWTH
+ROUND(SUM(SALES_AMOUNT) - (LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date)) * 100
+    /NULLIF((LAG(SUM(sales_amount)) OVER(ORDER BY MONTH(due_date))),0)),2)) GROWTH
 FROM gold_layer.fact_sales
 GROUP BY MONTH(due_date)
 
