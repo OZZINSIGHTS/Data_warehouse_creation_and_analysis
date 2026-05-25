ef1b7648c1d62d1687afc7dc74986e05a648e2d6
Oussama ZafZaf <oussamazafzaff@gmail.com>
2026-05-25 21:13:01 +0100

    Add total sales metrics queries to tests.sql
    Added queries to calculate various sales metrics.

--- scripts/tests.sql
+++ scripts/tests.sql
@@ -24,3 +24,22 @@ SELECT
     ,2) AS growth_percentage
 
 FROM sales_cte;
+
+-----------------------------------------------------------
+----------TOTAL SALES-------
+SELECT 'sales_amount' AS measure_value, SUM(sales_amount) measure_value FROM gold_layer.fact_sales
+UNION ALL
+SELECT 'total_item_sold' , SUM(quantity) FROM gold_layer.fact_sales
+UNION ALL
+SELECT 'avg_price' , AVG(price)  FROM gold_layer.fact_sales
+UNION ALL
+SELECT 'total_orders', COUNT(DISTINCT order_number) FROM gold_layer.fact_sales
+UNION ALL
+SELECT 'total_number_of_products', COUNT(DISTINCT product_key)  FROM gold_layer.dim_products
+UNION ALL
+SELECT 'number_of_customers', COUNT(DISTINCT customer_key)  FROM gold_layer.dim_customers
+UNION ALL
+SELECT 'number_of_active_customers', COUNT(DISTINCT customer_key)  FROM gold_layer.fact_sales
+
+
+
