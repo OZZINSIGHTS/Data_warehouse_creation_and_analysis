d49ebd6894d9314f064afcf193ea84f7183373d0
Oussama ZafZaf <oussamazafzaff@gmail.com>
2026-05-26 18:12:38 +0100

    Update SQL query for total sales over years
    Replaced the old sales activity query with a new total sales query.

--- scripts/information from data warehouse.sql
+++ scripts/information from data warehouse.sql
@@ -1,12 +1,9 @@
------------activity of the company over years-------------
-SELECT YEAR(a.due_date),b.category ,
-    FORMAT(SUM(a.sales_amount), 'C0') AS salesamount
-FROM gold_layer.fact_sales a
-LEFT JOIN 
-gold_layer.dim_products b
-ON a.product_key = b.product_key
-GROUP BY YEAR(a.due_date), b.category
-ORDER BY year(a.due_date), b.category
+-----------total sales of the company over years-------------
+SELECT YEAR(order_date) YEARS , SUM(sales_amount) SALESAMOUNT
+FROM gold_layer.fact_sales 
+WHERE YEAR(order_date) IS NOT NULL 
+GROUP BY YEAR(order_date) 
+ORDER BY  YEAR(order_date) 
 -------------------------------------------------------
 
 -----------activity of the company over years-------------
