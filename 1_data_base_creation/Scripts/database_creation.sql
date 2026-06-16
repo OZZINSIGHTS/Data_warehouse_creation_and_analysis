fee4e70352598e1bb51490a242ed4abf7686a994
Oussama ZafZaf <oussamazafzaff@gmail.com>
2026-06-16 11:31:59 +0100

    Enhance comments in database_creation.sql
    Added comments to clarify the purpose and results of the database creation script.

--- 1_data_base_creation/Scripts/database_creation.sql
+++ 1_data_base_creation/Scripts/database_creation.sql
@@ -1,3 +1,16 @@
+--------------database creation step---------------
+
+
+--PURPOSE : -Create the data base in which where the data base will be stored and schemas
+--            for the 'Medallion architecture'
+
+--This file Checks if the data base exists if yes; it is going to Log out all other users and rollback all their actions
+--     before creating the database and schemas
+
+--RESULTS : -DB: "Data_warehouse"
+--            Schemas: "bronze_layer" , "silver_layer", "gold_layer"
+
+-----------------Script----------------------------
 IF DB_ID('Data_warehouse') IS NOT NULL
 BEGIN
     ALTER DATABASE Data_Warehouse 
@@ -17,3 +30,4 @@ go
 create schema silver_layer
 go
 create schema gold_layer
+------------End of Script---------------------
